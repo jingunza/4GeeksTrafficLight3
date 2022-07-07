295015287ffa1f65e4dd65fa30978f6273550cbf
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-07-07 13:10:07 +0000

    semaforo con boton

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -3,18 +3,27 @@ import '../../styles/semaforo.css';
 
 import Foco from './foco.jsx';
 
-const TrafficLight = () => {
+const TrafficLight = (props) => {
     /*----------useState------------------------------------*/
-    const [lightsOn, setLightsOn] = useState(0);
+    const [lightColor, setLightColor] = useState(0);
     
     /*---------HTML-----------------------------------------*/
     return (
-    <div className="container col-3">
-        <div className="row d-flex justify-content-center">
-            <div id="semaforo" className="col-3 bg-dark justify-content-center">
-                <Foco clique={()=>setLightsOn('red')} color="red" estado={(lightsOn=='red')? "active" : "inactive"} />
-                <Foco clique={()=>setLightsOn('yellow')} color="yellow" estado={(lightsOn=='yellow')? "active" : "inactive"} />
-                <Foco clique={()=>setLightsOn('green')} color="green" estado={(lightsOn=='green')? "active" : "inactive"} />
+    <div>
+        <div className="container col-3">
+            <div className="row d-flex justify-content-center">
+                <div id="semaforo" className="col-3 bg-dark justify-content-center">
+                    <Foco clique={()=>setLightColor('red')} color="red" estado={(lightColor=='red')? "active" : "inactive"} />
+                    <Foco clique={()=>setLightColor('yellow')} color="yellow" estado={(lightColor=='yellow')? "active" : (lightColor=='other')? props.interm : "inactive"} />
+                    <Foco clique={()=>setLightColor('green')} color="green" estado={(lightColor=='green')? "active" : "inactive"} />
+                </div>
+            </div>
+        </div>
+        <div className="row align-items-center" style={{height: '100px'}}>
+            <div className="col-2 text-center container">
+                <button onClick={()=>{setLightColor('other')}} className="btn btn-warning text-danger">
+                    Intermitencia
+                </button>
             </div>
         </div>
     </div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
@@ -9,17 +9,23 @@ import "../styles/index.css";
 import TrafficLight from "./component/semaforo.jsx";
 
 const App = () => {
+    let iterador = timer%2;
+    const estados = ["inactive", "active"];
     return (
         <div className="container">
             <div className="row d-flex justify-content-center">
                 <div id="poste" className="col-1 bg-dark"></div>
             </div>
             <div className="row d-flex justify-content-center">
-                <TrafficLight />
+                <TrafficLight interm={estados[iterador]}/>
             </div>
         </div>
     );
 };
 
 //render your react application
-ReactDOM.render(<App />, document.querySelector("#app"));
+let timer = 0;
+setInterval(()=>{
+    timer++;
+    ReactDOM.render(<App />, document.querySelector("#app"));
+    },500);
